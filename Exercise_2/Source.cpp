197b84bbef6df5333ef4ce2a7e7d88d6b6a93843
Mahmed amer <67114727+Mahmedamer@users.noreply.github.com>
2020-11-24 16:22:01 +0200

    Exercise 2

--- Exercise_2/Source.cpp
+++ Exercise_2/Source.cpp
@@ -5,30 +5,36 @@ int* doubler(int a[], int size, int& newSize)
 {
 	newSize = 0;
 	for (int i = 0;i < size;i++)
+	{
 		if (a[i] != 0)
 			newSize++;
+	}
 	int* b = new int[newSize];
 	int j = 0;
-	for (int i = 0;i < newSize;i++)
+	for (int i = 0;i < size;i++)
+	{
 		if (a[i] != 0)
 			b[j++] = a[i] * a[i];
+	}
 	return b;
-	// this function takes as an input int array "a" of size "s"
-	// the function generates a new array
-	// and every element in this new array is the double value of its corresponding
-	//element in the array "a"
-		// Zero elements in the original array should not appear in the new array
-		// the function returns a pointer to the doubled array.
-		// The input array should not be changed
 }
 // Excerise 2 Main of doubler
 int main()
 {
-	// the main function
-	// generates an integer array
-	// gets the array elements from the user
-	// then it calls the "doubler" function
-	// then it prints the doubled array.
+	int n;
+	cout << "Enter The Array Number of Elements" << endl;
+	cin >> n;
+	int* A = new int[n];
+	cout << "Enter The Array Elements : ";
+	for (int i = 0;i < n;i++)
+		cin >> A[i];
+	int newn;
+	int* B = doubler(A, n,newn);
+	for (int i = 0;i < newn;i++)
+		cout << B[i] << " ";
+	cout << endl;
+	delete[] A;
+	delete[] B;
 	return 0;
 }
-// make sure you deallocate any dynamically allocated memory
+
